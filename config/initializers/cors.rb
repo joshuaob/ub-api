97f2cfae3b8aaee0dd36edba7017b4b71e5f6a58
Josh Obaro <joshuaaob@gmail.com>
2022-10-20 23:48:17 +0100

    WIP

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4201'
+    origins 'http://localhost:4201, https://app-staging.utterbounty.io'
     resource '*', headers: :any, methods: [:get, :post]
   end
 end
